0ca54428284f9adc81dbc5df566f0d10f43c11cf
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2023-03-14 18:07:25 +0000

    Compilinf Figure 1 panels

--- src/plotting/1_peptide.py
+++ src/plotting/1_peptide.py
@@ -33,9 +33,15 @@ if not os.path.exists(output_folder):
 # Set default font parameters
 font = {'family': 'normal',
         'weight': 'normal',
-        'size': 16}
+        'size': 20}
 matplotlib.rc('font', **font)
 plt.rcParams['svg.fonttype'] = 'none'
+
+palette = {
+    '':'',
+    '':'',
+    '':'',
+}
 
 # =====================Organise data=====================
 # Read in summary FOV data
@@ -59,32 +65,55 @@ mean_number_spots[['sample', 'concentration']
 # =================Read in example images=================
 
 example_BSA = imread(f'{root_path}data/peptide_images/X6Y1R3W3_641.tif')
+example_BSA = np.mean(example_BSA[10:, :, :], axis=0)
 example_monomer = imread(f'{root_path}data/peptide_images/X4Y1R3W2_641.tif')
+example_monomer = np.mean(example_monomer[10:, :, :], axis=0)
 example_dimer = imread(f'{root_path}data/peptide_images/X1Y1R2W2_641.tif')
+example_dimer = np.mean(example_dimer[10:, :, :], axis=0)
+
 
 # ==================Generate main figure==================
 
 fig, axes = plt.subplots(1, 4, figsize=(20, 5))
 
 # Panel D: Example BSA image
-example_BSA_mapped, _, _ = colorify.colorify_by_hex(mean_img_A, cmap_hex='#FF00FF', rescale_type='limits',
-                                       limits=[0,5000])
-axes[0].imshow()
+microim1 = microshow(images=[example_BSA],
+                               cmaps=['Greys'], flip_map=[True],
+                               label_color='black', ax=axes[0],
+                               unit='um', scalebar_size_in_units=10, scalebar_unit_per_pix=0.107, scalebar_font_size=0,)
+microim1 = microshow(images=[example_monomer],
+                               cmaps=['Greys'], flip_map=[True],
+                               label_color='black', ax=axes[2],
+                               unit='um', scalebar_size_in_units=10, scalebar_unit_per_pix=0.107, scalebar_font_size=0,)
+microim1 = microshow(images=[example_dimer],
+                     cmaps=['Greys'], flip_map=[True],
+                     label_color='black', ax=axes[1],
+                     unit='um', scalebar_size_in_units=10, scalebar_unit_per_pix=0.107, scalebar_font_size=0,)
 
 # Panel E: Spot count
 df = mean_number_spots[(mean_number_spots['concentration'].isin(['02', '', 'low'])) & (mean_number_spots['capture'] == 'HT7')]
-sns.stripplot(data=df, x='sample', y='spots_count', color='darkgrey', s=10)
+sns.stripplot(
+    data=df, 
+    x='sample', 
+    y='spots_count', 
+    color='#36454F',
+    s=10
+)
 sns.barplot(
     data=df,
     x='sample',
     y='spots_count',
     capsize=0.2,
     errwidth=2,
+    color='darkgrey'
 )
-axes[3].ylim(0, 400)
-axes[3].ylabel("Mean number of spots per FOV")
+axes[3].set_ylim(0, 400)
+axes[3].set_ylabel("Mean spots per FOV")
+axes[3].set_xlabel("")
 
-plt.savefig(f'{output_folder}mean_number_spots_HT7.svg')
+plt.tight_layout()
+plt.savefig(f'{output_folder}1_peptide.svg')
+plt.show()
 
 
 ########plot AT8 peptide data for supplementals #########
