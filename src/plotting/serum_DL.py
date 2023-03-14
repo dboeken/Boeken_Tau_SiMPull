e954b787c9f6a0e5afdb68b528ba7348bb99f0bd
dboeken <107995213+dboeken@users.noreply.github.com>
2023-03-14 17:18:53 +0000

    updated serum script

--- src/plotting/serum_DL.py
+++ src/plotting/serum_DL.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from smma.src import visualise
+#from smma.src import visualise
 from loguru import logger
 
 logger.info('Import OK')
